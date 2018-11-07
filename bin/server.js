0e7ed862c030566d8df29d64d69a590fdccb62e3
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2018-11-07 18:12:49 +0100

    added new filepaths

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ var app = express();
 const ServerSignaling = require('OpenHPI-P2P_CDN/src/server/signaling');
 
 const clientPath = './web_app';
-const p2pPath = './node_modules/OpenHPI-P2P_CDN/web_app'
+const p2pPath = './node_modules/OpenHPI-P2P_CDN/build/src'
 const port = 8080;
 
 app.use(express.static(clientPath));
