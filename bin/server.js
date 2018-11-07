e9cefb44d67b6a79728a7aee9837fb7367b6f34a
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2018-11-07 17:59:09 +0100

    first commit

---
+++ bin/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+const ServerSignaling = require('OpenHPI-P2P_CDN/src/server/signaling');
+
+const clientPath = './web_app';
+const p2pPath = './node_modules/OpenHPI-P2P_CDN/web_app'
+const port = 8080;
+
+app.use(express.static(clientPath));
+app.use(express.static(p2pPath));
+const signaling = new ServerSignaling();
+
+var server = app.listen(port, function () {
+  console.log('Example app listening on port '+port);
+});
+
+signaling.start(server);
