3e626ae926b7bc765bdd13270c64c292578b66be
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2018-11-26 14:00:40 +0100

    changed port binding

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const ServerSignaling = require('webrtc-p2p-cdn/src/server/signaling');
 
 const clientPath = './web_app';
 const p2pPath = './node_modules/webrtc-p2p-cdn/build/src'
-const port = 8080;
+const port = process.env.PORT || 80;
 
 app.use(express.static(clientPath));
 app.use(express.static(p2pPath));
