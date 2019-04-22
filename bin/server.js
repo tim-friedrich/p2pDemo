84b2870fd3598841fec1aabcb483c0ee01f15867
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2019-04-22 14:56:59 +0200

    added statistical tracking

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var app = express();
-const ServerSignaling = require('webrtc-p2p-cdn/src/server/signaling');
+var bodyParser = require("body-parser");
+
+
+const FayeServerSignaling = require('webrtc-p2p-cdn/src/server/fayeSignaling');
 
 const clientPath = './web_app';
-const p2pPath = './node_modules/webrtc-p2p-cdn/build/src'
+const p2pPath = './node_modules/webrtc-p2p-cdn/build/src';
+const fayePath = './node_modules/faye/client';
+const logPath = './logs/log.csv'
+const fastcsv = require('fast-csv');
+const fs = require('fs');
+
 const port = process.env.PORT || 80;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(clientPath));
 app.use(express.static(p2pPath));
-const signaling = new ServerSignaling();
+app.use(express.static(fayePath));
 
+// const signaling = new ServerSignaling();
 var server = app.listen(port, function () {
   console.log('Example app listening on port '+port);
 });
+app.post('/logs',function(request,response){
+  var body = request.body;
+  console.log(body);
+  const logHandle = fs.createWriteStream(logPath, { flags: 'a'});
+  fastcsv
+    .write(body, { headers: false, includeEndRowDelimiter: true })
+    .pipe(logHandle);
+  response.end("OK");
+});
 
-signaling.start(server);
+const fayeSignaling = new FayeServerSignaling(server);
+// signaling.start(server);
