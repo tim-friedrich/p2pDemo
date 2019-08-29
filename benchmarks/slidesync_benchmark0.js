24ddc47a47394386a287106fdd701fb19209e2d5
EC2 Default User <ec2-user@ip-172-31-36-52.eu-central-1.compute.internal>
2019-08-29 11:42:48 +0000

    ...

--- benchmarks/slidesync_benchmark0.js
+++ benchmarks/slidesync_benchmark0.js
@@ -3,7 +3,7 @@ const rootUrl = 'https://staging.slidesync.com/VnkRR2qkDa'
 
 // var browsers = [];
 var pages = [];
-var numberOfPeers = 2;
+var numberOfPeers = 30;
 var browser;
 var browsers = [];
 function Sleep(milliseconds) {
@@ -42,7 +42,7 @@ function launchPeer(id){
       // const browser = await puppeteer.launch({headless: true, executablePath: 'google-chrome'});
     // const context = await browser.createIncognitoBrowserContext();
     // var _browser = await puppeteer.launch({headless: false, executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'});
-    var _browser = await puppeteer.launch({headless: false, executablePath: 'google-chrome'});
+    var _browser = await puppeteer.launch({headless: true, executablePath: 'google-chrome'});
     browsers.push(_browser);
 
     // var page = await context.newPage()
