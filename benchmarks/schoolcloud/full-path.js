074fbda3dcfb2f6ee3ec765452bdaf065f956957
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2019-09-03 12:49:14 +0200

    rerun failed clients

--- benchmarks/schoolcloud/full-path.js
+++ benchmarks/schoolcloud/full-path.js
@@ -4,7 +4,7 @@ const helper = require('./helper.js')
 const courseID = '5d5c041d815e2e012ec7078f'
 var browsers = [];
 var pages = [];
-var numPeers = 30;
+var numPeers = 10;
 // var intervalEnd = 5;
 var peers = []
 var runNum = process.argv[2];
@@ -34,37 +34,43 @@ function rampUp () {
     resolve()
   })
 }
+function runTest(peerId, timeout) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      var _peer = {
+        peerId: peerId,
+        timeout: timeout
+      }
+      var peer = await helper.launchPeer(_peer);
+      var page = peer.page
+      await helper.signIn(peer);
+      await helper.sleep(2000)
+      console.log('Navigating peer ' + peer.peerId + ' to /courses/')
+      await page.click('[href="/courses/"]');
+      await helper.sleep(3000)
+      console.log('Navigating peer ' + peer.peerId + ' to Course details')
+      await page.click('a[href="/courses/5d6d25287f289200133e37d7"].sc-card-header .sc-card-title');
+      await helper.sleep(2000);
+      console.log('Navigating peer ' + peer.peerId + ' to Course Topic')
+      await page.click('.card.card-topic');
+      peers.push(peer)
+      resolve();
+    } catch (e) {
+      console.log(e);
+      console.log("rerun client: " + peerId)
+      await runTest(peerId);
+      resolve()
+    }
+  })
+}
 function testClient(peerId) {
   return new Promise(async function (resolve, reject){
     var timeout = helper.randomNumberInInterval(1000, intervalEnd*1000);
-
-
     console.log(timeout);
 
     setTimeout(async function(){
-      try {
-        var _peer = {
-          peerId: peerId,
-          timeout: timeout
-        }
-        var peer = await helper.launchPeer(_peer);
-        var page = peer.page
-        await helper.signIn(peer);
-        await helper.sleep(2000)
-        console.log('Navigating peer ' + peer.peerId + ' to /courses/')
-        await page.click('[href="/courses/"]');
-        await helper.sleep(3000)
-        console.log('Navigating peer ' + peer.peerId + ' to Course details')
-        await page.click('a[href="/courses/5d6d25287f289200133e37d7"].sc-card-header .sc-card-title');
-        await helper.sleep(2000);
-        console.log('Navigating peer ' + peer.peerId + ' to Course Topic')
-        await page.click('.card.card-topic');
-        peers.push(peer)
-        resolve();
-      } catch (e) {
-        console.log(e);
-        resolve();
-      }
+      await runTest(peerId, timeout);
+      resolve()
     }, timeout);
 
   })
